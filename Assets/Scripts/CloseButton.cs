0988c29c69d6fc8c118eff6dcd7b50e15c4a5795
PeriklisP <v8192218@live.tees.ac.uk>
2020-08-29 21:20:03 +0100

    Close buttons implemented

---
+++ Assets/Scripts/CloseButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CloseButton : MonoBehaviour
+{
+    public GameObject[] panels;
+    private Button button;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(Clicked);
+    }
+
+    public void Clicked()
+    {
+        if (button.name.Contains("Exit"))
+        {
+            Application.Quit();
+            Debug.LogError("Cant close device");
+        }
+        else
+        {
+            foreach (var panel in panels)
+            {
+                panel.SetActive(false);
+                if(panel == panels[0])
+                {
+                    panel.SetActive(true);
+                }
+            }
+        }
+    }
+}

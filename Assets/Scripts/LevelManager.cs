511992e8e8bc950b47ee17a9d46066d9465f06c9
PeriklisP <v8192218@live.tees.ac.uk>
2020-09-05 17:56:56 +0100

    Two scene-change AR/Map

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,10 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    public void SwitchCameraMode(Camera AR)
-    {
+    //public Camera cameraMap, cameraAR;
 
+    /*private void Awake()
+    {
+        cameraAR.enabled = false;
+        cameraAR.gameObject.SetActive(false);
+        cameraMap.enabled = true;
     }
+    public void SwitchCameraMode()
+    {
+        if (cameraMap.enabled)
+        {
+            cameraMap.gameObject.SetActive(false);
+            cameraMap.enabled = false;
+            cameraAR.gameObject.SetActive(true);
+            cameraAR.enabled = true;
+        }
+        else if (cameraAR.enabled)
+        {
+            cameraAR.gameObject.SetActive(false);
+            cameraAR.enabled = false;
+            cameraMap.gameObject.SetActive(true);
+            cameraMap.enabled = true;
+        }
+    }*/
     public void LoadLevelWithIndex(int levelIndex)
     {
         SceneManager.LoadScene(levelIndex, LoadSceneMode.Single);

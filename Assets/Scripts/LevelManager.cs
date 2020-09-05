6030fb9b55bdaf3f3d9e7499627066d67bdf3ee4
PeriklisP <v8192218@live.tees.ac.uk>
2020-09-05 11:13:43 +0100

    Map and AR into one Scene

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void SwitchCameraMode(Camera AR)
+    {
+
+    }
+    public void LoadLevelWithIndex(int levelIndex)
+    {
+        SceneManager.LoadScene(levelIndex, LoadSceneMode.Single);
+    }
+}

2c59b5184773c654688e773dcd2bf65d0137ca5a
PeriklisP <v8192218@live.tees.ac.uk>
2020-09-08 11:16:13 +0100

    Config Changes and Multiple detection
    Removed unused Vuforia assets
    Changed Vuforia config to detect multiple image/objects (4 for now)
    Added textures for testing
    Changed TrashCameraWindow camera borders/frames
    TrashCamera window renamed as RubbishCameraWindow to match UK dictionary
    Added the rubbish/coins into RubbishWindow and changed the button text to BACK

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,31 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    //public Camera cameraMap, cameraAR;
-
-    /*private void Awake()
-    {
-        cameraAR.enabled = false;
-        cameraAR.gameObject.SetActive(false);
-        cameraMap.enabled = true;
-    }
-    public void SwitchCameraMode()
-    {
-        if (cameraMap.enabled)
-        {
-            cameraMap.gameObject.SetActive(false);
-            cameraMap.enabled = false;
-            cameraAR.gameObject.SetActive(true);
-            cameraAR.enabled = true;
-        }
-        else if (cameraAR.enabled)
-        {
-            cameraAR.gameObject.SetActive(false);
-            cameraAR.enabled = false;
-            cameraMap.gameObject.SetActive(true);
-            cameraMap.enabled = true;
-        }
-    }*/
     public void LoadLevelWithIndex(int levelIndex)
     {
         SceneManager.LoadSceneAsync(levelIndex, LoadSceneMode.Single);

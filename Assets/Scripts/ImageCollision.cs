5d8d3b639621c77dc1c45e1b0ee72590a8057147
PeriklisP <v8192218@live.tees.ac.uk>
2020-09-09 16:35:52 +0100

    Cleaning Up

---
+++ Assets/Scripts/ImageCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageCollision : MonoBehaviour
+{
+    public Material matG, matR;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Contains("in"))
+        {
+            this.gameObject.GetComponent<MeshRenderer>().material = matG;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name.Contains("in"))
+        {
+            this.gameObject.GetComponent<MeshRenderer>().material = matR;
+        }
+    }
+}

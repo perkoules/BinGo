3a45fee33945f521170148953b104de10f8b899a
PeriklisP <v8192218@live.tees.ac.uk>
2020-09-10 21:35:12 +0100

    POI distance simple testing

---
+++ Assets/LocationDistance.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LocationDistance : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public float distance;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        text = GameObject.FindGameObjectWithTag("ttt").GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(text == null)
+        {
+            text = GameObject.FindGameObjectWithTag("ttt").GetComponent<TextMeshProUGUI>();
+        }
+        distance = Vector3.Distance(this.gameObject.transform.position, player.transform.position);
+        text.text = distance.ToString();
+    }
+}

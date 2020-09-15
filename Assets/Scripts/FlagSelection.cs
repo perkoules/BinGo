2493ca9ab164668f855bc5c86105169e0fe8b733
PeriklisP <v8192218@live.tees.ac.uk>
2020-09-15 23:05:44 +0100

    Db and UI
    Successful database communication (find user, create new one, etc)
    UI for log in [not finished]

---
+++ Assets/Scripts/FlagSelection.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class FlagSelection : MonoBehaviour
+{
+    public Sprite noneSelected;
+    public Image selectedFlag;
+    public string selectedCountryName;
+    public Dictionary<string, Sprite> countries = new Dictionary<string, Sprite>();
+
+    private void Start()
+    {
+        selectedCountryName ="Greece";
+        Image[] imagesObj = gameObject.GetComponentsInChildren<Image>();
+        foreach (var item in imagesObj)
+        {
+            countries.Add(item.sprite.name, item.sprite);
+        }
+    }
+
+    private void Update()
+    {
+        if (!countries.ContainsKey(selectedCountryName))
+        {
+            selectedFlag.sprite = noneSelected;
+        }
+        else
+        {
+            selectedFlag.sprite = countries[selectedCountryName];
+        }
+    }
+}
---
+++ Assets/Scripts/WebDB.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class WebDB : MonoBehaviour
+{
+    public string getUsersUrl, getLoginUrl, getRegisterUrl;
+    void Start()
+    {
+        //StartCoroutine(GetUsers(getUsersUrl));        
+        //StartCoroutine(Register("Periklis","admin21"));        
+    }
+
+    IEnumerator GetUsers(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(getUsersUrl))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(pages[page] + ": Error: " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+            }
+        }
+    }
+
+    IEnumerator Login(string username, string password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("loginUser", username);
+        form.AddField("loginPass", password);
+
+        using (UnityWebRequest www = UnityWebRequest.Post(getLoginUrl, form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+            }
+        }
+    }
+
+    IEnumerator Register(string username, string password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("loginUser", username);
+        form.AddField("loginPass", password);
+
+        using (UnityWebRequest www = UnityWebRequest.Post(getRegisterUrl, form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+            }
+        }
+    }
+}

fb474e73f26eefab4097042722ca89149655feec
PeriklisP <v8192218@live.tees.ac.uk>
2020-10-09 10:59:39 +0100

    Update MeasureDistance.cs

--- Assets/Scripts/MeasureDistance.cs
+++ Assets/Scripts/MeasureDistance.cs
@@ -1,6 +1,7 @@
 ﻿using Mapbox.CheapRulerCs;
 using Mapbox.Unity.Location;
 using Mapbox.Utils;
+using System.Linq;
 using UnityEngine;
 
 public class MeasureDistance : MonoBehaviour
@@ -8,22 +9,24 @@ public class MeasureDistance : MonoBehaviour
     public DeviceLocationProvider locationProvider;
     public SpawnBinsOnMap spawnBins;
     private double[] from, to;
-    public int binToLookFor = 0;
-    public double distance;
+    public double[] distances;
     private Vector2d playerCurrentLocation, toBinLocation;
 
     private void Start()
     {
+        distances = new double[11];
         InvokeRepeating("GetDistanceToBin", 4.0f, 3.0f);
     }
-
     private void GetDistanceToBin()
     {
         playerCurrentLocation = locationProvider.CurrentLocation.LatitudeLongitude;
-        toBinLocation = spawnBins._locations[binToLookFor];
         from = playerCurrentLocation.ToArray();
-        to = toBinLocation.ToArray();
         CheapRuler cr = new CheapRuler(from[0], CheapRulerUnits.Meters);
-        distance = cr.Distance(from, to);
+        for (int i = 0; i < spawnBins._locations.Length; i++)
+        {
+            toBinLocation = spawnBins._locations[i];
+            to = toBinLocation.ToArray();
+            distances[i] = cr.Distance(from, to);
+        }
     }
 }

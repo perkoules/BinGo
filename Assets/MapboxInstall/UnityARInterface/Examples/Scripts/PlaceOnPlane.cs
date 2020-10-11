1c8c5364e15cb405e007ae600bfca868722e6f1c
PeriklisP <v8192218@live.tees.ac.uk>
2020-10-12 00:02:41 +0100

    Adjustments
    Changed UI images from 256 to 192 in the main screen
    Added the new main screen and deleted the old
    Placing object on planes

--- Assets/MapboxInstall/UnityARInterface/Examples/Scripts/PlaceOnPlane.cs
+++ Assets/MapboxInstall/UnityARInterface/Examples/Scripts/PlaceOnPlane.cs
@@ -2,25 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityARInterface;
+using Mapbox.Unity.MeshGeneration.Interfaces;
 
 public class PlaceOnPlane : ARBase
 {
     [SerializeField]
-    private Transform m_ObjectToPlace;
+    private GameObject m_ObjectToPlace;
 
     void Update ()
     {
+#if UNITY_EDITOR
         if (Input.GetMouseButton(0))
         {
             var camera = GetCamera();
 
             Ray ray = camera.ScreenPointToRay(Input.mousePosition);
 
-			int layerMask = 1 << LayerMask.NameToLayer("ARGameObject"); // Planes are in layer ARGameObject
+            int layerMask = 1 << LayerMask.NameToLayer("ARGameObject"); // Planes are in layer ARGameObject
 
             RaycastHit rayHit;
             if (Physics.Raycast(ray, out rayHit, float.MaxValue, layerMask))
-                m_ObjectToPlace.transform.position = rayHit.point;
+                Instantiate(m_ObjectToPlace, rayHit.point, Quaternion.identity);
+            //m_ObjectToPlace.transform.position = rayHit.point;
         }
+#endif
+#if UNITY_ANDROID
+        if (Input.touchCount == 1)
+        {
+            Touch myTouch = Input.GetTouch(0);
+            
+            var camera = GetCamera();
+
+            Ray ray = camera.ScreenPointToRay(myTouch.position);
+
+            int layerMask = 1 << LayerMask.NameToLayer("ARGameObject"); // Planes are in layer ARGameObject
+
+            RaycastHit rayHit;
+            if (Physics.Raycast(ray, out rayHit, float.MaxValue, layerMask))
+                Instantiate(m_ObjectToPlace, rayHit.point, Quaternion.identity);
+            //m_ObjectToPlace.transform.position = rayHit.point;
+        }
+#endif
     }
 }

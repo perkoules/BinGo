5a75147db53557498b0929552a215f5c23830530
PeriklisP <v8192218@live.tees.ac.uk>
2020-10-24 23:33:23 +0100

    Destroy Monsters
    On touch screen

--- Assets/GoogleARCore/Examples/HelloAR/Scripts/HelloARController.cs
+++ Assets/GoogleARCore/Examples/HelloAR/Scripts/HelloARController.cs
@@ -138,13 +138,8 @@ namespace GoogleARCore.Examples.HelloAR
                         prefab = GameObjectHorizontalPlanePrefab;
                     }
 
-                    // Instantiate prefab at the hit pose.
-                    bool ok = true;
-                    if (ok)
-                    {
-                        var gameObject = Instantiate(prefab, hit.Pose.position, hit.Pose.rotation);
-                        ok = false;
-                    }
+                    var gameObject = Instantiate(prefab, hit.Pose.position, hit.Pose.rotation);
+                    
                     // Compensate for the hitPose rotation facing away from the raycast (i.e.
                     // camera).
                     gameObject.transform.Rotate(0, k_PrefabRotation, 0, Space.Self);
---
+++ Assets/Scripts/MonsterDestroyer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterDestroyer : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity))
+            {
+                if (hit.transform.gameObject.name.Contains("Monster"))
+                {
+                    Destroy(hit.transform.gameObject);
+                }
+            }
+        }
+    }
+}

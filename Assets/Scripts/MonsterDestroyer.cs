23d946aa7743d69f56f69e8009bad4e1bd0553c1
PeriklisP <v8192218@live.tees.ac.uk>
2020-10-25 14:11:08 +0000

    Monster Interaction

--- Assets/Scripts/MonsterDestroyer.cs
+++ Assets/Scripts/MonsterDestroyer.cs
@@ -3,19 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MonsterDestroyer : MonoBehaviour
-{
-    
+{    
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) || Input.GetTouch(0).phase == TouchPhase.Began)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity))
             {
-                if (hit.transform.gameObject.name.Contains("Monster"))
+                GameObject go = hit.transform.gameObject;
+                if (go.name.Contains("Monster"))
                 {
-                    Destroy(hit.transform.gameObject);
+                    go.GetComponent<Animator>().SetBool("IsDead", true);
+                    Destroy(go, 3f);
                 }
             }
         }

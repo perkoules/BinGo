ce0b8d71875af61262d344d2069720feaada508e
PeriklisP <v8192218@live.tees.ac.uk>
2020-10-26 19:31:51 +0000

    Badge Explanation/ Notifications

---
+++ Assets/BadgeExplanation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class BadgeExplanation : MonoBehaviour
+{
+    [SerializeField] 
+    private GameObject prefabNotification;
+    [SerializeField]
+    private string textToShow;
+    [SerializeField]
+    private RectTransform parent;
+    private Button btn;
+    private TextMeshProUGUI myText;
+
+
+    void Awake()
+    {
+        btn = GetComponent<Button>();
+        myText = prefabNotification.GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    void Start()
+    {
+        btn.onClick.AddListener(InitNotification);
+    }
+
+    private void InitNotification()
+    {
+        myText.text = textToShow;
+        GameObject go = Instantiate(prefabNotification, parent);
+        Destroy(go, 2f);
+    }
+}

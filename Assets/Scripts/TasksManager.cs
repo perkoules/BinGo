b7884abca07428388d61cde3e9fab4a372a69719
PeriklisP <v8192218@live.tees.ac.uk>
2020-10-27 23:23:36 +0000

    Basic Task Controller

---
+++ Assets/Scripts/TasksManager.cs
@@ -0,0 +1,63 @@
+﻿using PlayFab;
+using PlayFab.ClientModels;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class TasksManager : MonoBehaviour
+{
+    private DateTime dateCreated, dateToday;
+    private int daysPassed = 0;
+    public RectTransform parent;
+    public GameObject[] taskPrefabs;
+    private Dictionary<int, string> tasks;
+
+    private void Awake()
+    {
+        tasks = new Dictionary<int, string>()
+        {
+            {1, "Collect 2 waste" },
+            {2, "Collect 2 recycle" },
+            {3, "Collect 2 recycle" },
+        };
+    }
+
+    private void Start()
+    {
+        GetDateCreated();
+    }
+
+    private void GetDateCreated()
+    {
+        PlayFabClientAPI.GetAccountInfo(
+            new GetAccountInfoRequest { },
+            result =>
+            {
+                dateCreated = result.AccountInfo.Created;
+                dateToday = DateTime.Today;
+                daysPassed = (dateToday - dateCreated).Days;
+                Debug.Log("Passed " + daysPassed);
+                StartCoroutine(ShowTask());
+            },
+            error => Debug.LogError(error.GenerateErrorReport()));        
+    }
+
+    IEnumerator ShowTask()
+    {
+        yield return new WaitUntil(() => Time.timeSinceLevelLoad > 30);
+        if(daysPassed >=0 && daysPassed < 30)
+        {
+            for (int i = 0; i < taskPrefabs.Length; i++)
+            {
+                GameObject go = Instantiate(taskPrefabs[i], parent);
+                TextMeshProUGUI goText = go.GetComponentInChildren<TextMeshProUGUI>();
+                goText.text = tasks.ElementAt(i).Value;
+            }
+        }
+    }
+
+
+}

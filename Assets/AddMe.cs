000fde13e3779c981a2fb49519b085d2f5c13de6
PeriklisP <v8192218@live.tees.ac.uk>
2020-11-29 21:41:00 +0000

    Spawn Enemies
    For scavenger hunt

---
+++ Assets/AddMe.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddMe : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        CollectionTest.Instance.AddEnemy(this.gameObject);
+    }
+
+}
---
+++ Assets/CollectionTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectionTest : MonoBehaviour
+{
+    public List<GameObject> enemiesAdded;
+    public static CollectionTest Instance { get; private set; }
+    private void OnEnable()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    private void Awake()
+    {
+        enemiesAdded = new List<GameObject>();
+    }
+
+    public void AddEnemy(GameObject go)
+    {
+        if(!enemiesAdded.Exists(g => g.name == go.name))
+        {
+            enemiesAdded.Add(go);
+        }
+    }
+}

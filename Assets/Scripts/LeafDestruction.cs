c01f57181c82bc32f769cc5acab2ec4581699d6c
PeriklisP <v8192218@live.tees.ac.uk>
2021-01-05 13:10:53 +0000

    Object Pooling

---
+++ Assets/Scripts/LeafDestruction.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeafDestruction : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.SetActive(false);
+        ObjectPooler.Instance.Reposition(collision.gameObject);
+    }
+}
--- Assets/Scripts/LeafSpawner.cs
+++ Assets/Scripts/LeafSpawner.cs
@@ -1,37 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class LeafSpawner : MonoBehaviour
 {
-    public GameObject leavesHolder;
-    public List<GameObject> leaves;
-    private PolygonCollider2D col;
-    private void Awake()
+    private void Start()
     {
-        col = leavesHolder.GetComponent<PolygonCollider2D>();
+        InvokeRepeating("EnableLeaves", 1f, 0.5f);
     }
 
-    void Start()
+    private void EnableLeaves()
     {
-        InvokeRepeating("Spawner", 0.1f, 3f); 
-    }
-
-    private void Spawner()
-    {
-        for (int i = 0; i < 2; i++)
+        GameObject go = ObjectPooler.Instance.GetPooledObject("Orange Leaf");
+        if (go != null)
         {
-            GameObject go = Instantiate(leaves[Random.Range(0, leaves.Count)], WhereToSpawn(col.bounds), Quaternion.identity, leavesHolder.transform);
+            go.SetActive(true);
         }
     }
 
-    public Vector3 WhereToSpawn(Bounds bounds)
-    {
-        return new Vector3(
-        Random.Range(bounds.min.x, bounds.max.x),
-        Random.Range(bounds.min.y, bounds.max.y),
-        Random.Range(bounds.min.z, bounds.max.z)
-    );
-    }
-
+    
 }
